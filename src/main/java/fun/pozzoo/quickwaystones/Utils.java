c122458e130a9b3f2fe97f0c869022742eb70e1e
Tautvydas <tautvydas.gaming@gmail.com>
2025-07-31 05:25:42 +0300

    Add formatAgoTime method

--- src/main/java/fun/pozzoo/quickwaystones/Utils.java
+++ src/main/java/fun/pozzoo/quickwaystones/Utils.java
@@ -43,4 +43,26 @@ public class Utils {
         if (!chunk.isLoaded())
             chunk.load();
     }
+
+    public static String formatAgoTime(long millis) {
+        long totalSeconds = (System.currentTimeMillis() - millis) / 1000;
+        long days = totalSeconds / 86400;
+        long hours = (totalSeconds % 86400) / 3600;
+        long minutes = (totalSeconds % 3600) / 60;
+        long seconds = totalSeconds % 60;
+        StringBuilder sb = new StringBuilder();
+        if (days > 0) {
+            sb.append(days).append("d");
+            if (hours > 0) sb.append(" ").append(hours).append("h");
+        } else if (hours > 0) {
+            sb.append(hours).append("h");
+            if (minutes > 0) sb.append(" ").append(minutes).append("m");
+        } else if (minutes > 0) {
+            sb.append(minutes).append("m");
+            if (seconds > 0) sb.append(" ").append(seconds).append("s");
+        } else {
+            sb.append(seconds).append("s");
+        }
+        return sb.toString();
+    }
 }
