545db73561029788783c1d65b876e7464e5e3d83
Tautvydas <tautvydas.gaming@gmail.com>
2025-07-31 05:24:28 +0300

    Fill out load/save data methods

--- src/main/java/fun/pozzoo/quickwaystones/managers/PlayerDataManager.java
+++ src/main/java/fun/pozzoo/quickwaystones/managers/PlayerDataManager.java
@@ -1,24 +1,76 @@
 package fun.pozzoo.quickwaystones.managers;
 
 import fun.pozzoo.quickwaystones.QuickWaystones;
+import fun.pozzoo.quickwaystones.data.PlayerData;
+import fun.pozzoo.quickwaystones.data.WaystoneData;
+import fun.pozzoo.quickwaystones.enums.PlayerSortType;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
-public class PlayerDataManager extends DataManagerBase {
+public class PlayerDataManager extends DataManagerBase<Map<UUID, PlayerData>> {
+    private final Map<UUID, PlayerData> playerDataMap = new HashMap<>();
+
     public PlayerDataManager(QuickWaystones plugin) throws IOException {
         super(plugin, "players");
     }
 
-    public void loadData() {
-
+    @Override
+    public void loadData() throws IOException, InvalidConfigurationException {
+        load();
+        Map<String, WaystoneData> map = getPlugin().getSimpleWaystonesMap();
+        for (String key : keys) {
+            UUID uuid = UUID.fromString(key);
+            PlayerData playerData = new PlayerData(
+                    uuid,
+                    PlayerSortType.valueOf(config.getString(key + ".sorting.type")),
+                    config.getBoolean(key + ".sorting.inverted"),
+                    config.getStringList(key + ".sorting.list").stream().map(map::get).toList()
+            );
+            playerDataMap.put(uuid, playerData);
+        }
     }
 
+    @Override
     public void saveData() {
+        config = new YamlConfiguration();
+        for (PlayerData playerData : playerDataMap.values()) {
+            config.set(playerData.getUniqueId().toString() + ".sorting.type", playerData.getSortType().toString());
+            config.set(playerData.getUniqueId().toString() + ".sorting.inverted", playerData.isSortingInverted());
+            config.set(playerData.getUniqueId().toString() + ".sorting.list", playerData.getSortedWaystones()
+                    .stream()
+                    .map(WaystoneData::getUniqueId)
+                    .toList());
+        }
+        save();
+    }
 
+    @Override
+    public Map<UUID, PlayerData> getData() {
+        return playerDataMap;
+    }
+
+    public PlayerData createData(Player player, boolean save) {
+        PlayerData data = new PlayerData(
+                player.getUniqueId(),
+                PlayerSortType.valueOf(getPlugin().getConfig().getString("WaystoneScreen.DefaultPlayerSorting.Type")),
+                getPlugin().getConfig().getBoolean("WaystoneScreen.DefaultPlayerSorting.Inverted"),
+                getPlugin().getWaystonesMap().values().stream()
+                        .filter(x -> x.isOwner(player) ||
+                                x.getAddedPlayers().contains(player.getUniqueId()) ||
+                                x.isInternal() ||
+                                x.isGloballyAccessible())
+                        .toList()
+        );
+        if (save) {
+            getPlugin().getPlayerDataMap().put(player.getUniqueId(), data);
+            saveData();
+        }
+        return data;
     }
 }
